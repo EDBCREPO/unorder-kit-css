eb0c2b7c5ca196c3c140f4be4ac93c8e05d06b4c
Enmanuel D Becerra C <becerracenmanueld@gmail.com>
2025-12-08 22:28:58 -0400

    update

--- compile.sh
+++ compile.sh
@@ -1,3 +1,22 @@
-time g++ -o main main.cpp -I./Modules -lssl -lcrypto -lz 
+#!/bin/bash
 
-./main ; ./main ?mode=test
+clear
+
+FLAG="-lcrypto -lssl -lz" # -largon2
+NAME="service"
+FILE=$(mktemp)
+
+if [ ! -d "./build" ] ; then 
+    mkdir "./build"
+fi
+
+echo -e "Killing Service\n" ; $( killall $NAME )
+
+echo -e "Compiling Service\n"
+if !( g++ -o ./build/$NAME ./service/$NAME.cpp -I ./include $FLAG ) 2> "$FILE"; then
+      echo -e "\n" ; cat "$FILE" >&2 ; exit
+else
+      echo -e "Done"
+fi
+
+echo -e "\nRunning Service" ; ./build/$NAME; ./build/$NAME ?mode=test
